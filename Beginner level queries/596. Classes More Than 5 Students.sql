cd6389b974070eadfe64e45fcdb06ee1c165eefa
Said Ahmadli <68443078+saidahmadli@users.noreply.github.com>
2023-06-23 23:20:34 +0400

    Add files via upload

---
+++ Beginner level queries/596. Classes More Than 5 Students.sql
@@ -0,0 +1,28 @@
+-- 596. Classes More Than 5 Students
+
+-- There is a table courses with columns: student and class
+
+-- Please list out all classes which have more than or equal to 5 students.
+
+-- For example, the table:
+
+-- +---------+------------+
+-- | student | class      |
+-- +---------+------------+
+-- | A       | Math       |
+-- | B       | English    |
+-- | C       | Math       |
+-- | D       | Biology    |
+-- | E       | Math       |
+-- | F       | Computer   |
+-- | G       | Math       |
+-- | H       | Math       |
+-- | I       | Math       |
+-- +---------+------------+
+
+-- Solution
+
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING count(distinct student)>=5
